a922569b38768b67c237a0ce4d009da96e5f5879
jiubuntu <rlawldn0412@gmail.com>
2024-01-17 09:54:40 +0900

    Initial Commit

---
+++ WhatToDoToday_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatToDoToday_ios
+//
+//  Created by 김지우 on 1/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
