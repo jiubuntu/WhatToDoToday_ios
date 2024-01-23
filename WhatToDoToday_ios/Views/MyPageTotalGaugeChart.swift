2172b6329af189f24827cd9c69c72be39a0a3811
jiubuntu <rlawldn0412@gmail.com>
2024-01-23 17:29:53 +0900

    20240123 commit - UI 1차 개발 완료

---
+++ WhatToDoToday_ios/Views/MyPageTotalGaugeChart.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageTotalGaugeChart.swift
+//  WhatToDoToday_ios
+//
+//  Created by 김지우 on 1/23/24.
+//
+
+import Foundation
---
+++ WhatToDoToday_ios/Views/MyPageWeekGaugeChart.swift
@@ -0,0 +1,8 @@
+//
+//  MyPageWeekGaugeChart.swift
+//  WhatToDoToday_ios
+//
+//  Created by 김지우 on 1/23/24.
+//
+
+import Foundation
