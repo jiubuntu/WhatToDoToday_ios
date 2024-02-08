3e7c50c9f545f70debf306e15148304b7c3d577b
jiubuntu <rlawldn0412@gmail.com>
2024-02-08 23:48:09 +0900

    20240208 commit - ToDo 삭제기능 완료, ToDo 완료기능 완료

---
+++ WhatToDoToday_ios/Utils/Delegates.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+
+protocol ToDoDetailDelegate: AnyObject {
+    // MARK: - Todo 데이터를 업데이트하고 원래화면으로 돌아왔을때, 원래화면을 업데이트 하기 위함
+    func didUpdateToDo()
+}
+
+
+
+protocol ToDoListCellDelegate: AnyObject {
+    // MARK: - 테이블뷰 셀에서 toDoMark를 눌렀을때의 예외처리를 위한 alert창 표시
+    func showAlert()
+}
--- WhatToDoToday_ios/VIewModels/ToDoDetailDelegate.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ToDoDetailDelegate.swift
-//  WhatToDoToday_ios
-//
-//  Created by 김지우 on 2/7/24.
-//
-
-import Foundation
